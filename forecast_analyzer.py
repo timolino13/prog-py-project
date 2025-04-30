2a97c678bae5e3d087aa09085fb8b7f0f60d3b83
timo <scarabelt@gmail.com>
2025-04-30 11:40:38 +0200

    Analyze forecast

---
+++ forecast_analyzer.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class ForecastAnalyzer:
+    def __init__(self, forecast_data):
+        self.df = self.create_pandas_dataframe(forecast_data)
+
+    def create_pandas_dataframe(self, forecast_data):
+        forecast_list = forecast_data.get("list", [])
+        df = pd.DataFrame(
+            [
+                {
+                    "timestamp": entry["dt"],
+                    "temperature": entry["main"]["temp"],
+                    "feels_like": entry["main"]["feels_like"],
+                    "humidity": entry["main"]["humidity"],
+                }
+                for entry in forecast_list
+            ]
+        )
+
+        df["timestamp"] = pd.to_datetime(df["timestamp"], unit="s")
+        return df
+
+    def plot_chart(self):
+        fig, axes = plt.subplots(2, 1, figsize=(10, 10))
+
+        sns.lineplot(ax=axes[0], x="timestamp", y="temperature", data=self.df, color="tab:red", label="Temperature")
+        sns.lineplot(ax=axes[0], x="timestamp", y="feels_like", data=self.df, color="tab:orange", label="Feels Like")
+        sns.lineplot(ax=axes[1], x="timestamp", y="humidity", data=self.df, color="tab:blue", label="Humidity")
+
+        axes[0].set_xlabel("Day")
+        axes[0].set_ylabel("Temperature", color="tab:red")
+        axes[0].set_title("Temperature Forecast")
+        axes[0].legend()
+
+        axes[1].set_xlabel("Day")
+        axes[1].set_ylabel("Humidity", color="tab:blue")
+        axes[1].set_title("Humidity Forecast")
+        axes[1].legend()
+
+        plt.tight_layout()
+        plt.show()
+
+    def analyze(self):
+        self.plot_chart()
+
+        # Analyze the forecast data
+        print("Analyzing 5 days forecast data...")
+
+        # Count the number of entries
+        print(f"\nNumber of forecast entries: {len(self.df)}")
+
+        # Add a date column
+        self.df["date"] = self.df["timestamp"].dt.date
+
+        # Calculate average temperature per day
+        daily_avg_temps = self.df.groupby("date")["temperature"].mean()
+
+        coldest_day_date = daily_avg_temps.idxmin()
+        coldest_day_average_temp = self.df[self.df["date"] == coldest_day_date]["temperature"].mean()
+        print("\nColdest day:")
+        print(
+            f"Date: {coldest_day_date}, Average Temperature: {coldest_day_average_temp:.2f}°C, ")
+
+        daily_max_temps = self.df.groupby("date")["temperature"].max()
+        hottest_day_date = daily_max_temps.idxmax()
+        hottest_temp = self.df[self.df["date"] == hottest_day_date]["temperature"].max()
+        print("\nHottest day:")
+        print(
+            f"Date: {hottest_day_date}, Average Temperature: {hottest_temp:.2f}°C, ")
+
+        # Calculate average temperature
+        avg_temp = self.df["temperature"].mean()
+        print(f"\nAverage temperature: {avg_temp:.2f}°C")
+
+        # Calculate average humidity
+        avg_humidity = self.df["humidity"].mean()
+        print(f"\nAverage humidity: {avg_humidity:.2f}%")
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from weather_analyzer import WeatherAnalyzer
+from forecast_analyzer import ForecastAnalyzer
 from weather_data_fetcher import *
 
 fetcher = WeatherDataFetcher("56b0e25aa21d726d7d7ed712f76bf41c")
-
 forecast = fetcher.fetch_forecast("Rapperswil")
 
-print(forecast)
-
-analyzer = WeatherAnalyzer(forecast)
-
-analyzer.plot_chart()
+analyzer = ForecastAnalyzer(forecast)
+analyzer.analyze()
